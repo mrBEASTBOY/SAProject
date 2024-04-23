f6fcf4c4907f3b0a7067ad16df905d8c117b48ce
mrBEASTBOY <75973622+mrBEASTBOY@users.noreply.github.com>
2024-04-23 13:44:24 -0500

    linh v1
    Push code

---
+++ Project/ProductApplication/src/main/java/com/ProductApplication/ProductApplication/dto/ProductDTO.java
@@ -0,0 +1,59 @@
+package com.ProductApplication.ProductApplication.dto;
+
+public class ProductDTO {
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private int productPrice;
+
+    public ProductDTO() {}
+
+    public ProductDTO(int productId, String productName, String productDescription, int productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productPrice=" + productPrice +
+                '}';
+    }
+}
---
+++ Project/StoreApplication/src/test/java/com/StoreApplication/StoreApplication/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StoreApplication.StoreApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
